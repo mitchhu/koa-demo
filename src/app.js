f9bfbf3ac085b9528d2030809bdb3a4b397bbdbc
humingchao <humingchao@yit.com>
2022-04-23 15:39:23 +0800

    初始化koa项目

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const Koa = require('koa')
+
+const app = new Koa()
+
+app.use((ctx, next) => {
+  ctx.body = 'Hello World'  
+})
+
+app.listen(5000, () => {
+  console.log('server is running on http://localhost:5000')
+})
