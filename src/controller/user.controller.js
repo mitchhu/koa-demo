5ae642a829988b18fe05ce244d3230ccd7ac5558
humingchao <humingchao@yit.com>
2022-04-24 11:17:08 +0800

    颁发token

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,5 +1,8 @@
-const { createUser } = require('../service/user.service')
+const jwt = require('jsonwebtoken')
+const { createUser, getUserInfo } = require('../service/user.service')
 const { userRegisterError } = require('../constant/error.type')
+
+const { JWT_SECRET } = require('../config/config.default')
 
 class userController {
   
@@ -17,7 +20,7 @@ class userController {
 
       // 返回结果
       ctx.body = {
-        code: '10000',
+        code: 0,
         message: '用户注册成功',
         result: {
           user_name: res.user_name
@@ -32,8 +35,21 @@ class userController {
   async login(ctx, next) {
     const { user_name } = ctx.request.body
 
-    ctx.body = `欢迎回来，${user_name}`
-
+    // 获取用户信息（在token的playload中，记录id，user_name, is_admin）
+    try {
+      // 剔除passwor属性，将剩余的属性放在res对象中
+      const {password, ...res} = await getUserInfo({ user_name })
+      ctx.body = {
+        code: 0,
+        message: '用户登录成功',
+        result: {
+          token: jwt.sign(res, JWT_SECRET, {expiresIn: '1d'})
+        }
+      }
+    } catch (error) {
+      console.log('用户登录失败', error)
+      return
+    }
   }
 }
 
