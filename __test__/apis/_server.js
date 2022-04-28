8d56fcccd1ae6e178d16892e55396e5f5774ab6b
humingchao <humingchao@yit.com>
2022-04-28 15:37:47 +0800

    单元测试

---
+++ __test__/apis/_server.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+// const supertest = require('supertest');
+
+
+// const server = require('../../src/app').callback();
+// const request = supertest(server);
+// request.get(url).query(bodyOrParams).set(headers).expect(status).end(done);
+
+
+module.exports = {
+
+  async get(url) {
+    const res = await axios.get(url);
+    return res;
+  }
+}
+
+
---
+++ __test__/demo.test.js
@@ -0,0 +1,6 @@
+
+describe('test demo', () => {
+  test('1 + 1 = 2', () => {
+    expect(1 + 1).toBe(2)
+  })
+})
